22a574408c22c3ffb99e7e04ddd19c0295e17eef
adakri <71411281+adakri@users.noreply.github.com>
2021-02-08 21:39:08 +0100

    Update charge.cpp

--- charge.cpp
+++ charge.cpp
@@ -19,8 +19,26 @@ void charge_a(int n, int Np, int me, int& begin, int& end )
 
 void charge_b(int n, int Np, int me, int& begin, int& end)
 {
-  int q=floor((n-begin)/(Np-me));
+  int q=floor(n/Np);
+  begin=end+1;
+  end=begin+q-1;
+  if(me==12)
+  {
+    end=n-1;
+  }
+}
 
+void charge_c(int n, int Np, int me, int& begin, int& end) //max - min=0
+{
+  int q=floor(n/Np)+1;
+  begin=end+1;
+  end=begin+q-1;
+  if(me==12)
+  {
+    end=n-1;
+  }
+
+  
 }
 
 
@@ -31,10 +49,32 @@ int main()
     int end,begin;
     end=-1;
     begin=-1;
+    cout<<"distribution a"<<endl;
     for (int me=0; me<Np; me++)
     {
       charge_a(n,Np,me,begin,end);
-      cout<<"charge du processeur "<<me<<" {"<<begin<<" "<<end<<"}"<<endl;
+      cout<<"charge du processeur "<<me<<" {"<<begin<<" , "<<end<<"}"<<" decompte "<<end-begin<<endl;
+    }
+
+
+    cout<<"distribution b"<<endl;
+    Np=13;
+    end=-1;
+    begin=-1;
+    for (int me=0; me<Np; me++)
+    {
+      charge_b(n,Np,me,begin,end);
+      cout<<"charge du processeur "<<me<<" {"<<begin<<" , "<<end<<"}"<<" decompte "<<end-begin<<endl;
+    }
+
+    cout<<"distribution c"<<endl;
+    Np=13;
+    end=-1;
+    begin=-1;
+    for (int me=0; me<Np; me++)
+    {
+      charge_c(n,Np,me,begin,end);
+      cout<<"charge du processeur "<<me<<" {"<<begin<<" , "<<end<<"}"<<" decompte "<<end-begin<<endl;
     }
     //cout<<"donner le nombre de processeurs!"<<endl;
     //cin>>Np;
